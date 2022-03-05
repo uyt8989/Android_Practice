42fc07fa48e6f3a68e8c1e61ad544dc0025336b9
movie <kyungyun98@naver.com>
2022-03-05 21:03:24 +0900

    class01

---
+++ src/example/myapp/Aquarium.kt
@@ -0,0 +1,25 @@
+package example.myapp
+
+class Aquarium (var length: Int = 100, var width : Int = 20, var height : Int = 40){
+    var volume : Int
+        get() = width * height * length / 1000
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
+    init {
+        println("aquarium initializing")
+    }
+    constructor(numberOfFish: Int) : this() {
+        // 2,000 cm^3 per fish + extra room so water doesn't spill
+        val tank = numberOfFish * 2000 * 1.1
+        height = (tank / (length * width)).toInt()
+    }
+
+    fun printSize() {
+        println("Width : $width cm " +
+                "Length : $length cm " +
+                "Height : $height cm")
+        println("Volume : $volume liters")
+    }
+
+}
